f1d6a96f79ce75ac85a4ebb048d8c53b1bf895b7
Maria Mafalda Fernando <maria.fernando@ocado.com>
2019-06-18 11:07:42 +0100

    deploying portal branch

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ rbenv rehash
 pip install git+https://github.com/PyGithub/PyGithub.git@ba50af5
 pip install "urllib3==1.22" --force-reinstall
 
-pip install -t lib codeforlife-portal
+pip install git+https://github.com/ocadotechnology/codeforlife-portal.git@test_permissions_on_dv
 if [ "$ENVIRONMENT" = "default" ]
 then
     pip install -t lib --upgrade --no-deps aimmo
