787aebdff20840eafb2747398eb53d2a220af51b
Maria Mafalda Fernando <maria.fernando@ocado.com>
2019-06-19 08:41:57 +0100

    testing aimmo fix

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ rbenv rehash
 pip install git+https://github.com/PyGithub/PyGithub.git@ba50af5
 pip install "urllib3==1.22" --force-reinstall
 
+pip install -t lib codeforlife-portal
 pip install git+https://github.com/ocadotechnology/aimmo.git@trying_to_fix_permissions
 
 python get_latest_aimmo_unity_release.py
